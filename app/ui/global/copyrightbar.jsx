7e7ca72993eb3f5433d8945e4b395b266eaf3a97
akhilbandaruu <akhilbandaru@gmail.com>
2024-03-23 16:50:03 +0530

    CSS Changes

--- app/ui/global/copyrightbar.jsx
+++ app/ui/global/copyrightbar.jsx
@@ -6,7 +6,9 @@ export default function CopyrightBar() {
     <div className="copyright-section">
       <div>Copyrights © 2024 Qcentrio, All Rights Reserved.</div>
       <Image src={logo} alt="" />
-      <div className="handcrafted">Handcrafted with ❤ by X-Verity</div>
+      <div className="handcrafted">
+        Handcrafted with ❤ by <a href="https://www.x-verity.com">X-Verity</a>
+      </div>
     </div>
   );
 }
