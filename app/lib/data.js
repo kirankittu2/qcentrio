17aa279854d3182c19e7586fec9363a3078d3765
VSH Kiran <saiharikiran@outlook.com>
2024-03-24 12:42:39 +0530

    Added titles to cards in perspective page

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,21 +1,22 @@
 const blogs = [
   {
-    title: "Title",
+    title: "7 ways to maximize your cloud security",
     content: "Content",
     link: "/perspectives/7-ways-to-maximize-your-cloud-security",
   },
   {
-    title: "Title",
+    title: "Boost your enterprise innovation with robotic process automation",
     content: "Content",
     link: "/perspectives/boost-your-enterprise-innovation-with-robotic-process-automation",
   },
   {
-    title: "Title",
+    title: "How blockchain redefines business and banking",
     content: "Content",
     link: "/perspectives/how-blockchain-redefines-business-and-banking",
   },
   {
-    title: "Title",
+    title:
+      "Navigating the cloud landscape: 5 key strategies for effective cloud assessment",
     content: "Content",
     link: "/perspectives/navigating-the-cloud-landscape",
   },
