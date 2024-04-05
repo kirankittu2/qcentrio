58c2471a279bce9208ac07ce242196e0d4c00e16
akhilbandaruu <akhilbandaru@gmail.com>
2024-04-05 12:58:22 +0530

    minor change

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
-      <Cursor />
+      {/* <Cursor /> */}
     </html>
   );
 }
