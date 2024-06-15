64922a5c7d5d3904140527e5a7215704606e1b30
VSH Kiran <saiharikiran@outlook.com>
2024-06-15 07:09:32 +0530

    email config modified

--- app/config/mail-config.js
+++ app/config/mail-config.js
@@ -42,25 +42,25 @@ import nodemailer from "nodemailer";
 //   },
 // });
 
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: "qcentrio@gmail.com",
-//     pass: `rnoq tndw qnpb pxnv`,
-//   },
-// });
-
 const transporter = nodemailer.createTransport({
-  host: "mail.qcentrio.com",
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    user: "qcadmin",
-    pass: `Dj"Hi4PaJt9Kt_^`,
+    user: "qcentrio@gmail.com",
+    pass: `rnoq tndw qnpb pxnv`,
   },
 });
+
+// const transporter = nodemailer.createTransport({
+//   host: "mail.qcentrio.com",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: "qcadmin",
+//     pass: `Dj"Hi4PaJt9Kt_^`,
+//   },
+// });
 
 // const transporter = nodemailer.createTransport({
 //   host: "smtp.office365.com",
